2bac75f7c43efe7a610a0dd4b4c17441c3070f5d
Tasdi <tasdikul@student.chalmers.se>
2017-12-11 21:00:43 +0100

    Created a test ot check if you get the correct json from api

--- WeatherApplicationTest/RequestHandlerTest.cs
+++ WeatherApplicationTest/RequestHandlerTest.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using NUnit;
-using Moq;
+﻿using Moq;
+using Newtonsoft.Json;
 using NUnit.Framework;
 using WeatherApplication.Services;
 using WeatherApplication.Models;
@@ -28,5 +27,20 @@ namespace WeatherApplicationTest
                 "api.openweathermap.org/data/2.5/weather?q=hawai&APPID=2c807f909d802a67435deb6a813afe1c");
 
         }
+
+        [Test]
+        public void ShouldGetCorrectJsonFromApi()
+        {
+            string apiUrl1 = requestHandler.GetApiUrl("london");
+            string result1 = requestHandler.getJsonFromApi(apiUrl1);
+            weatherInformation = JsonConvert.DeserializeObject<Mock<IWeatherInformation>>(result1);
+            Assert.AreEqual("London", weatherInformation.Name);
+
+            string apiUrl2 = requestHandler.GetApiUrl("stockholm");
+            string result2 = requestHandler.getJsonFromApi(apiUrl2);
+            weatherInformation = JsonConvert.DeserializeObject<Mock<IWeatherInformation>>(result2);
+            Assert.AreEqual("Stockholm", weatherInformation.Name);
+        }
+
     }
 }
